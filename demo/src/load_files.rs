8504ee6669cc3770480763865275616e122c8108
Pascal Hertleif <pascal@technocreatives.com>
2024-07-18 10:06:15 +0200

    demo: Load meta files

--- demo/src/load_files.rs
+++ demo/src/load_files.rs
@@ -1,4 +1,10 @@
-use bevy::prelude::*;
+use std::{
+    fmt::Write,
+    path::{Path, PathBuf},
+};
+
+use anyhow::{Context as _, Result};
+use bevy::{prelude::*, utils::HashMap};
 use bevy_materialx_importer::{MaterialX, MaterialXPlugin};
 
 pub struct LoadFilesPlugin;
@@ -9,6 +15,7 @@ impl Plugin for LoadFilesPlugin {
 
         app.add_plugins((MaterialXPlugin,))
             .insert_resource(filter)
+            .register_type::<ExampleFiles>()
             .add_systems(Startup, (load_example_files,));
     }
 }
@@ -21,14 +28,31 @@ struct MaterialFilter(Option<String>);
 #[derive(Debug, Resource, Reflect)]
 #[reflect(Resource)]
 #[allow(dead_code)]
-struct ExampleFiles(Vec<(String, Handle<MaterialX>)>);
+struct ExampleFiles(HashMap<String, MaterialAsset>);
+
+#[derive(Debug, Reflect)]
+struct MaterialAsset {
+    path: PathBuf,
+    material: Handle<MaterialX>,
+    meta: Option<Metadata>,
+}
+
+// Same as in downloader
+#[derive(Debug, serde::Deserialize, Reflect)]
+pub struct Metadata {
+    pub source: String,
+    pub name: String,
+    pub id: String,
+    pub url: String,
+    pub preview_image: Option<String>,
+}
 
 fn load_example_files(
     mut commands: Commands,
     assets: Res<AssetServer>,
     filter: Res<MaterialFilter>,
 ) {
-    let mut res = Vec::new();
+    let mut res = HashMap::new();
 
     let examples = glob::glob("assets/**/*.mtlx").unwrap();
     for example in examples {
@@ -40,13 +64,48 @@ fn load_example_files(
             }
         }
 
-        res.push((
-            name.clone(),
-            assets.load(path.strip_prefix("assets").unwrap().to_path_buf()),
-        ));
+        let meta = match load_meta_file(&path).context("load meta file") {
+            Ok(meta) => meta,
+            Err(e) => {
+                log_err(&e);
+                None
+            }
+        };
+
+        res.insert(
+            name,
+            MaterialAsset {
+                material: assets.load(path.strip_prefix("assets").unwrap().to_path_buf()),
+                meta,
+                path,
+            },
+        );
     }
 
     info!("found {} materials", res.len());
 
     commands.insert_resource(ExampleFiles(res));
+}
+
+fn load_meta_file(path: &Path) -> Result<Option<Metadata>> {
+    let meta_path = path.with_file_name("meta.json");
+    if meta_path.exists() {
+        let file = std::fs::read_to_string(&meta_path)
+            .with_context(|| format!("can't read {meta_path:?}"))?;
+        let meta: Metadata =
+            serde_json::from_str(&file).with_context(|| format!("can't parse {meta_path:?}"))?;
+        Ok(Some(meta))
+    } else {
+        Ok(None)
+    }
+}
+
+fn log_err(error: &anyhow::Error) {
+    let mut source = format!("{error}");
+    let mut e = error.source();
+    while let Some(inner) = e {
+        let _ = write!(&mut source, " > {inner}");
+        e = inner.source();
+    }
+    error!("{source}");
 }
